69ebd3ba38a13777b10a8873c4a7eef3bb029b67
Shintaro Kojima <goodies@codeout.net>
2024-08-18 15:21:44 +0900

    fix: upgrade eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import prettierPlugin from "eslint-config-prettier";
+import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
+import eslint from "@eslint/js";
+import tsEslint from "typescript-eslint";
+
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default tsEslint.config(
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  prettierPlugin,
+  {
+    "plugins": {
+      "simple-import-sort": simpleImportSortPlugin
+    },
+    "rules": {
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        {
+          "assertionStyle": "as"
+        }
+      ],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error"
+    }
+  }
+);
+
+
