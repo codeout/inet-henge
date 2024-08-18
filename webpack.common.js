409431f72df78e99b0191dcdab6a17f9b0535484
Shintaro Kojima <goodies@codeout.net>
2024-08-18 19:03:45 +0900

    fix(webpack): introduce BannerPlugin to write the version number to the output files

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,16 @@
+const webpack = require("webpack");
+
+const package = require("./package.json");
+
+const banner = `
+/*!
+ * inet-henge  v${package.version}
+ * @author ${package.author.name}
+ * @license ${package.license}
+ * Copyright (c) 2016-2024 Shintaro Kojima
+ */
+`;
+
 module.exports = {
   entry: "./src/diagram",
   output: {
@@ -20,4 +33,5 @@ module.exports = {
     d3: "d3",
   },
   devtool: "source-map",
+  plugins: [new webpack.BannerPlugin({ banner: banner.trim(), raw: true })],
 };
